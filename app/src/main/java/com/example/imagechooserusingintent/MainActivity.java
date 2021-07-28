569c8bda50fa6aae3dbb0b683275da6fb761d256
prashant <csprshantphuse@gmail.com>
2021-07-28 16:31:42 +0530

    replaced depricated methods

--- app/src/main/java/com/example/imagechooserusingintent/MainActivity.java
+++ app/src/main/java/com/example/imagechooserusingintent/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.imagechooserusingintent;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -29,11 +34,30 @@ Uri  uri;
         btn.setOnClickListener(v -> {
             Intent intent=new Intent(Intent.ACTION_PICK);
             intent.setType("image/*");
-            startActivityForResult(intent,1);
+            //startActivityForResult(intent,1);
+            resultLauncher.launch(intent);
         });
     }
-
+ActivityResultLauncher<Intent> resultLauncher=registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
     @Override
+    public void onActivityResult(ActivityResult result) {
+
+            Intent intent=result.getData();
+            uri=intent.getData();
+            try{
+                Bitmap bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),uri);
+                imageView.setImageBitmap(bitmap);
+
+            }catch (FileNotFoundException fnfe){
+                fnfe.printStackTrace();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+    }
+});
+  /*  @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==1 && resultCode==RESULT_OK && data!=null && data.getData()!=null){
@@ -49,5 +73,5 @@ Uri  uri;
              e.printStackTrace();
          }
         }
-    }
+    }*/
 }
